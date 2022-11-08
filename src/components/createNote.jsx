0c0606256d90744b8d205c67ab25b62b3909a027
nikjo987 <35563605+nikjo987@users.noreply.github.com>
2022-11-08 17:44:04 +0530

    Update createNote.jsx

--- src/components/createNote.jsx
+++ src/components/createNote.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import httpservice from "../services/httpService.js";
+import httpservice from "../services/httpService";
 
 const CreateNote = (props) => {
     let params = useParams()
