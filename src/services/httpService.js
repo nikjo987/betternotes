81c0cbe5d78d485c6d1ad9f55f3be3f55496c957
nikjo987 <35563605+nikjo987@users.noreply.github.com>
2022-11-08 17:12:33 +0530

    Update httpService.js

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,10 +1,24 @@
-import axios from 'axios'
+import axios from "axios";
 
-export default {
+axios.interceptors.request.use(
+    (config) => {
+        // config.baseURL = "http://localhost:3000"
+        config.baseURL = "https://nervous-snaps-moth.cyclic.app";//process.env.NODE_API_URI || 
+        return config;
+    },
+    (error) => {
+        return error;
+    }
+);
+
+let httpService =  {
     get: axios.get,
     post: axios.post,
     put: axios.put,
     delete: axios.delete,
     patch: axios.patch,
-    all: axios.all
-}
+    all: axios.all,
+};
+
+
+export default httpService;
