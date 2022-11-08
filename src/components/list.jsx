3229fde5fcd9c8ec8da0e868a7e2c68d40fae9d5
nikjo987 <35563605+nikjo987@users.noreply.github.com>
2022-11-08 17:05:05 +0530

    Update list.jsx

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -4,11 +4,7 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 const ListView = (props) => {
-  //   return (
-  //     <div className="list">        
-  //       {props.notes.map(note => <div className='list-contents' key={note.title}><Note note={note}/></div>)}      
-  //     </div>
-  // )
+  
   return (
     <Row xs={1} lg={2} className="g-4">
       {props.notes.map(note => (
